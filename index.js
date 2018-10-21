91a7ce777855c3fc090cf351e5960839cb3e23ea
Felix Böhm <me@feedic.com>
2018-10-21 15:14:55 -0700

    prettier

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var parse = require("./parse.js"),
     compile = require("./compile.js");
 
-module.exports = function nthCheck(formula){
-	return compile(parse(formula));
+module.exports = function nthCheck(formula) {
+    return compile(parse(formula));
 };
 
 module.exports.parse = parse;
